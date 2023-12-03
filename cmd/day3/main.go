a10ff2d5de0e742c2a041074d735656d9d9bbb34
Vishal Kotcherlakota <dishbreak@gmail.com>
2023-12-02 23:05:48 -0800

    day 3 refactor

--- cmd/day3/main.go
+++ cmd/day3/main.go
@@ -23,11 +23,11 @@ func main() {
 var matchPartNum = regexp.MustCompile(`\d+`)
 var matchSymbol = regexp.MustCompile(`[#*\/%@$&\+\-\=]`)
 
-func part1(r io.Reader) int {
+func parseSpaces(r io.Reader) (symbols map[image.Point]byte, parts map[image.Point]int) {
 	s := bufio.NewScanner(r)
 
-	symbols := make(map[image.Point]byte)
-	parts := make(map[image.Point]int)
+	symbols = make(map[image.Point]byte)
+	parts = make(map[image.Point]int)
 
 	for lineNo := 0; s.Scan(); lineNo++ {
 		line := s.Text()
@@ -47,7 +47,10 @@ func part1(r io.Reader) int {
 			}
 		}
 	}
+	return
+}
 
+func findPartNums(p image.Point, parts map[image.Point]int) []int {
 	ptNorth := image.Pt(0, -1)
 	ptSouth := image.Pt(0, 1)
 	ptEast := image.Pt(1, 0)
@@ -57,59 +60,83 @@ func part1(r io.Reader) int {
 	ptSouthEast := ptSouth.Add(ptEast)
 	ptSouthWest := ptSouth.Add(ptWest)
 
+	var result []int
+
+	addIfPresent := func(results []int, o image.Point) []int {
+		num, ok := parts[o]
+		if !ok {
+			return results
+		}
+		return append(results, num)
+	}
+
+	// look east and west first -- these are always seen once by the symbol
+	result = addIfPresent(result, p.Add(ptEast))
+	result = addIfPresent(result, p.Add(ptWest))
+
+	/*
+		with north and south, this gets messy. here are all the valid cases for north
+		...
+		.&.
+
+		.1.
+		.&.
+
+		12.
+		.&.
+
+		.12
+		.&.
+
+		123
+		.&.
+
+		1.1
+		.&.
+
+		1..
+		.&.
+
+		..1
+		.&.
+
+		if there's a number directly above, that's all that can be seen by the symbol
+		if there's *NOT* a number directly above, we might see numbers at both corners
+	*/
+
+	num, directlyAbove := parts[p.Add(ptNorth)]
+	if !directlyAbove {
+		result = addIfPresent(result, p.Add(ptNorthEast))
+		result = addIfPresent(result, p.Add(ptNorthWest))
+	} else {
+		result = append(result, num)
+	}
+
+	// same logic holds for south
+	num, directlyBelow := parts[p.Add(ptSouth)]
+	if !directlyBelow {
+		result = addIfPresent(result, p.Add(ptSouthEast))
+		result = addIfPresent(result, p.Add(ptSouthWest))
+	} else {
+		result = append(result, num)
+	}
+
+	return result
+}
+
+func part1(r io.Reader) int {
+	symbols, parts := parseSpaces(r)
+
 	acc := 0
 
 	// iterate over all the positions of symbols
 	for p := range symbols {
-		// look east and west first -- these are always seen once by the symbol
-		acc += parts[p.Add(ptEast)]
-		acc += parts[p.Add(ptWest)]
+		// find all parts surrounding the symbol
+		nums := findPartNums(p, parts)
 
-		/*
-			with north and south, this gets messy. here are all the valid cases for north
-			...
-			.&.
-
-			.1.
-			.&.
-
-			12.
-			.&.
-
-			.12
-			.&.
-
-			123
-			.&.
-
-			1.1
-			.&.
-
-			1..
-			.&.
-
-			..1
-			.&.
-
-			if there's a number directly above, that's all that can be seen by the symbol
-			if there's *NOT* a number directly above, we might see numbers at both corners
-		*/
-
-		num, directlyAbove := parts[p.Add(ptNorth)]
-		acc += num
-		if !directlyAbove {
-			acc += parts[p.Add(ptNorthEast)]
-			acc += parts[p.Add(ptNorthWest)]
+		for _, num := range nums {
+			acc += num
 		}
-
-		// same logic holds for south
-		num, directlyBelow := parts[p.Add(ptSouth)]
-		acc += num
-		if !directlyBelow {
-			acc += parts[p.Add(ptSouthEast)]
-			acc += parts[p.Add(ptSouthWest)]
-		}
-
 	}
 	return acc
 }
